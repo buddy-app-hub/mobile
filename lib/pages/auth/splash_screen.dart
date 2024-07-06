c7337d33d981e3755c7275e3f8e677dfee9da7d5
Maxi Romero <mromerov@fi.uba.ar>
2024-07-06 19:13:36 -0300

    Integration of Firebase Authentication and Token Sending to API (#3)
    * Added base config for firebase auth
    
    * Sign up integrado con firebase auth
    
    * login integrado con firebase auth
    
    * fix android dependencies
    
    * integration firebase and session instance
    
    * feat: add api service base with token
    
    ---------
    
    Co-authored-by: Agustin Gabrielli <agabrielli@fi.uba.ar>

---
+++ lib/pages/auth/splash_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:mobile/pages/home.dart';
+import 'package:provider/provider.dart';
+import 'package:mobile/pages/auth/providers/auth_session_provider.dart';
+import 'package:mobile/pages/auth/login.dart';
+
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<AuthSessionProvider>(
+      builder: (context, authProvider, _) {
+        if (authProvider.isAuthenticated) {
+          return HomePage();
+        }
+
+        return LoginPage();
+      });
+  }
+}
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,7 +1,10 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'package:mobile/pages/auth/providers/auth_session_provider.dart';
+import 'package:mobile/routes.dart';
+import 'package:mobile/services/api_service_base.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,18 +14,42 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
   String weatherIcon = "https://cdn.weatherapi.com/weather/64x64/day/113.png";
   String weatherCity = '?º';
   Future<void> getPost() async {
-    var uri = Uri.parse(
-        "http://api.weatherapi.com/v1/current.json?key=d35441289f874f678a135931240506&q=Bariloche&aqi=yes");
-    var response = await http.get(uri);
 
-    if (response.statusCode == 200) {
+    var jsonb = await ApiService.get<dynamic>(
+        endpoint: "http://api.weatherapi.com/v1/current.json",
+        params: {
+          "key": "d35441289f874f678a135931240506",
+          "q": "Bariloche",
+          "aqi": "yes"
+    });
+
+    var icon = jsonb["current"]["condition"]["icon"];
+    var locName = jsonb["location"]["name"];
+    var temp = jsonb["current"]["temp_c"].toString();
+
+    /*try {
+      var api = await ApiService.get<String>(endpoint: "http://localhost:8080/ping", params: {});
+      print(api);
+    } catch (e) {
+      print("Error");
+    }*/
+
+    setState(() {
+      weatherIcon = "https:${icon}";
+    });
+    setState(() {
+      weatherCity = "${locName} ${temp}º";
+    });
+
+    /*if (response.statusCode == 200) {
       String body = utf8.decode(response.bodyBytes);
       final data = jsonDecode(body);
       setState(() {
-        weatherIcon = "https:" + data["current"]["condition"]["icon"];
+        weatherIcon = "https:${data["current"]["condition"]["icon"]}";
       });
       setState(() {
         weatherCity = data["location"]["name"] +
@@ -32,11 +59,13 @@ class _HomePageState extends State<HomePage> {
       });
     } else {
       throw Exception("No funciono la conexion");
-    }
+    }*/
   }
 
   @override
   Widget build(BuildContext context) {
+    final authProvider = Provider.of<AuthSessionProvider>(context);
+
     return Scaffold(
       appBar: appBar(),
       body: Center(
@@ -47,6 +76,7 @@ class _HomePageState extends State<HomePage> {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
+                  Text(authProvider.user?.email ?? "No logueado"),
                   ElevatedButton(
                       onPressed: getPost, child: Text('Get current weather')),
                 ],
@@ -62,6 +92,13 @@ class _HomePageState extends State<HomePage> {
                   ),
                 ],
               ),
+            ),
+            IconButton(
+              icon: Icon(Icons.logout),
+              onPressed: () async {
+                await authProvider.signOut();
+                Navigator.pushReplacementNamed(context, Routes.login);
+              },
             ),
           ],
         ),
