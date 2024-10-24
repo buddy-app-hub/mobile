8dbef7e5f4139bdd0c92052056b9e400577c43b3
Tomás Macía <tmacia@fi.uba.ar>
2024-10-23 21:53:45 -0300

    Withdraw money from buddy wallet (#35)

--- lib/services/wallet_service.dart
+++ lib/services/wallet_service.dart
@@ -1,3 +1,4 @@
+import 'package:mobile/models/price.dart';
 import 'package:mobile/models/wallet.dart';
 import 'package:mobile/services/payment_api_service.dart';
 
@@ -5,6 +6,24 @@ class WalletService {
   Future<Wallet> getWallet(String id) async {
     var response = await PaymentApiService.get(
       endpoint: "/wallets/$id",
+    );
+
+    print(response);
+
+    return Wallet.fromJson(response);
+  }
+
+  Future<Wallet> withdraw(String id, Price price) async {
+    var response = await PaymentApiService.post(
+      endpoint: "/wallets/$id/transactions",
+      body: {
+        "amount": price.amount,
+        "currency": price.currencyId,
+        "description": "Retiro",
+        "payment_id": "withdrawal",
+        "status": "pending",
+        "type": "withdraw",
+      },
     );
 
     print(response);
