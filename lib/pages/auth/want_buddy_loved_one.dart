16b5d63751b41871f0d8e46616f3474d1c7bb0ec
Agustin Gabrielli <agabrielli@fi.uba.ar>
2024-10-29 20:40:03 -0300

    Dropdown for relationship with elder

--- lib/pages/auth/want_buddy_loved_one.dart
+++ lib/pages/auth/want_buddy_loved_one.dart
@@ -17,11 +17,23 @@ List<DropdownMenuItem<String>>? items = [
   DropdownMenuItem(value: 'Femenino', child: Text('Femenino')),
   DropdownMenuItem(value: 'No binario', child: Text('No binario')),
   DropdownMenuItem(value: 'Otro', child: Text('Otro')),
-  DropdownMenuItem(value: 'Prefiero no decir', child: Text('Prefiero no decir')),
+  DropdownMenuItem(
+      value: 'Prefiero no decir', child: Text('Prefiero no decir')),
+];
+
+List<DropdownMenuItem<String>>? relationshipToElderOptions = [
+  DropdownMenuItem(value: 'Hijo', child: Text('Hijo/a')),
+  DropdownMenuItem(value: 'Nieto', child: Text('Nieto/a')),
+  DropdownMenuItem(value: 'Amigo', child: Text('Amigo/a')),
+  DropdownMenuItem(value: 'Hermano', child: Text('Hermano/a')),
+  DropdownMenuItem(value: 'Sobrino', child: Text('Sobrino/a')),
+  DropdownMenuItem(value: 'Familiar lejano', child: Text('Familiar lejano')),
+  DropdownMenuItem(value: 'Otro', child: Text('Otro')),
 ];
 
 class WantBuddyForLovedOnePage extends StatefulWidget {
-  const WantBuddyForLovedOnePage({super.key, required this.countryCode, required this.phone});
+  const WantBuddyForLovedOnePage(
+      {super.key, required this.countryCode, required this.phone});
 
   final String phone;
   final String countryCode;
@@ -65,20 +77,17 @@ class _WantBuddyForLovedOnePageState extends State<WantBuddyForLovedOnePage> {
           gender: elderGenderController.text,
           birthDate: DateFormat('dd/MM/yyyy').parse(elderDateController.text),
         ),
-        phoneNumber: PhoneNumber(
-            countryCode: widget.countryCode,
-            number: widget.phone),
+        phoneNumber:
+            PhoneNumber(countryCode: widget.countryCode, number: widget.phone),
         registrationDate: DateTime.now(),
-        registrationMethod:
-            'email',
+        registrationMethod: 'email',
         email: authProvider.user!.email!,
         onLovedOneMode: true,
         lovedOne: LovedOne(
             firstName: firstNameController.text,
             lastName: lastNameController.text,
             phoneNumber: PhoneNumber(
-                countryCode: widget.countryCode,
-                number: widget.phone),
+                countryCode: widget.countryCode, number: widget.phone),
             email: authProvider.user!.email!,
             relationshipToElder: relationshipToElderController.text),
         elderProfile: ElderProfile(
@@ -111,7 +120,6 @@ class _WantBuddyForLovedOnePageState extends State<WantBuddyForLovedOnePage> {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    
 
     return Scaffold(
       appBar: AppBar(
@@ -207,7 +215,8 @@ class _WantBuddyForLovedOnePageState extends State<WantBuddyForLovedOnePage> {
                         items: items,
                         decoration: InputDecoration(
                           hintText: "Mi género",
-                          contentPadding: EdgeInsets.symmetric(horizontal: 12, vertical: 15),
+                          contentPadding: EdgeInsets.symmetric(
+                              horizontal: 12, vertical: 15),
                           border: OutlineInputBorder(
                             borderRadius: BorderRadius.circular(18),
                             borderSide: BorderSide.none,
@@ -217,10 +226,23 @@ class _WantBuddyForLovedOnePageState extends State<WantBuddyForLovedOnePage> {
                         ),
                       ),
                       const SizedBox(height: 16),
-                      TextFormField(
-                        controller: relationshipToElderController,
+                      DropdownButtonFormField<String>(
+                        onChanged: (value) {
+                          setState(() {
+                            genderController.text = value!;
+                          });
+                        },
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Ingresá tu género';
+                          }
+                          return null;
+                        },
+                        items: relationshipToElderOptions,
                         decoration: InputDecoration(
                           hintText: "Relación con tu ser querido",
+                          contentPadding: EdgeInsets.symmetric(
+                              horizontal: 12, vertical: 15),
                           border: OutlineInputBorder(
                             borderRadius: BorderRadius.circular(18),
                             borderSide: BorderSide.none,
@@ -228,12 +250,6 @@ class _WantBuddyForLovedOnePageState extends State<WantBuddyForLovedOnePage> {
                           fillColor: theme.colorScheme.primary.withOpacity(0.1),
                           filled: true,
                         ),
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Ingresá la relación con tu ser querido';
-                          }
-                          return null;
-                        },
                       ),
                     ],
                   ),
@@ -304,7 +320,8 @@ class _WantBuddyForLovedOnePageState extends State<WantBuddyForLovedOnePage> {
                         items: items,
                         decoration: InputDecoration(
                           hintText: "Género de tu ser querido",
-                          contentPadding: EdgeInsets.symmetric(horizontal: 12, vertical: 15),
+                          contentPadding: EdgeInsets.symmetric(
+                              horizontal: 12, vertical: 15),
                           border: OutlineInputBorder(
                             borderRadius: BorderRadius.circular(18),
                             borderSide: BorderSide.none,
@@ -328,7 +345,9 @@ class _WantBuddyForLovedOnePageState extends State<WantBuddyForLovedOnePage> {
                           fillColor: theme.colorScheme.primary.withOpacity(0.1),
                           filled: true,
                         ),
-                        validator: (value) => value == null || value.isEmpty ? 'Ingresá tu fecha de nacimiento' : null,
+                        validator: (value) => value == null || value.isEmpty
+                            ? 'Ingresá tu fecha de nacimiento'
+                            : null,
                       ),
                     ],
                   ),
@@ -351,8 +370,7 @@ class _WantBuddyForLovedOnePageState extends State<WantBuddyForLovedOnePage> {
           ),
           child: Text(
             "Listo",
-            style: TextStyle(
-                color: theme.colorScheme.onPrimary, fontSize: 20),
+            style: TextStyle(color: theme.colorScheme.onPrimary, fontSize: 20),
           ),
         ),
       ),
